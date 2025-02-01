1e344771712cd3cf724f0f8c5911fff0f131c724
jdcb4 <joedore@gmail.com>
2025-02-01 13:50:15 +1100

    Add instructions, leaderboard, and settings pages

---
+++ app/instructions/page.tsx
@@ -0,0 +1,8 @@
+export default function Instructions() {
+  return (
+    <div>
+      <h1>Instructions</h1>
+      <p>This is the instructions page.</p>
+    </div>
+  );
+}
---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Leaderboard() {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <p>This is the leaderboard page.</p>
+    </div>
+  );
+}
